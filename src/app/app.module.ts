ba1b066b2a1fd407955b997b0924fc9ed80d9620
buctwbzs <zh_buctwbzs@163.com>
2018-08-08 23:23:18 +0800

    :sparkles: Build the basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './home/profile/profile.component';
+import { ConcatComponent } from './home/concat/concat.component';
+import { WorksComponent } from './home/works/works.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NotMatchComponent } from './not-match/not-match.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    ConcatComponent,
+    WorksComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotMatchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/not-match/not-match.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-match',
+  templateUrl: './not-match.component.html',
+  styleUrls: ['./not-match.component.css']
+})
+export class NotMatchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
